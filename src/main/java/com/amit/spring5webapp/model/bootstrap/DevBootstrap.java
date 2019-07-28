f5b302c6641d6ee6885e35cea8f5ef196f211c02
amitmodi06 <amitmodi06@gmail.com>
2019-07-29 00:27:51 +0530

    Changes as per Assignment: • Create a new entity ‘Publisher’, with name and address attributes. • Remove publisher string from Book entity. Add Publisher use a @OneToOne relationship • Add Publisher repository. • Modify DevBootstrap to populate publisher / book entity. • Inspect DB in H2 Console

--- src/main/java/com/amit/spring5webapp/model/bootstrap/DevBootstrap.java
+++ src/main/java/com/amit/spring5webapp/model/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.amit.spring5webapp.model.bootstrap;
 
 import com.amit.spring5webapp.model.Author;
 import com.amit.spring5webapp.model.Book;
+import com.amit.spring5webapp.model.Publisher;
 import com.amit.spring5webapp.model.repositories.AuthorRepository;
 import com.amit.spring5webapp.model.repositories.BookRepository;
+import com.amit.spring5webapp.model.repositories.PublisherRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -17,10 +19,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository=publisherRepository;
     }
 
     @Override
@@ -32,20 +36,26 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Chetan
         Author chetan = new Author("Chetan", "Bhagat");
-        Book fps = new Book("Five Point Someone", "2345", "Penguin ");
+        Publisher peng = new Publisher("Penguin", "India");
+        Book fps = new Book("Five Point Someone", "2345", peng);
         chetan.getBooks().add(fps);
         fps.getAuthors().add(chetan);
 
+        publisherRepository.save(peng);
         authorRepository.save(chetan);
         bookRepository.save(fps);
 
+
         //robin
         Author robin = new Author("Robin", "Sharma");
-        Book monk = new Book("The Monk Who Sold His Ferrari", "1234", "Harper Collins");
+        Publisher harper = new Publisher("Harper Collins", "USA");
+        Book monk = new Book("The Monk Who Sold His Ferrari", "1234", harper);
         robin.getBooks().add(monk);
         monk.getAuthors().add(robin);
 
+        publisherRepository.save(harper);
         authorRepository.save(robin);
         bookRepository.save(monk);
+
     }
 }
